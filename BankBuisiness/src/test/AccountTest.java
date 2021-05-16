98b795b3d8dd22c726289416e08f5181b51ef275
hak2711 <hak271111@gmail.com>
2021-05-16 15:35:21 +0900

    Refactor: 입금, 출금 기능 정제

--- BankBuisiness/src/test/AccountTest.java
+++ BankBuisiness/src/test/AccountTest.java
@@ -1,20 +1,27 @@
 package test;
 
 import main.Account;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public class AccountTest {
+
+    private Account account;
+
+    @Before
+    public void setup() {
+        account = new Account(10000);
+    }
+
     @Test
     public void testAccount() throws Exception {
-        Account account = new Account(10000);
     }
 
     @Test
     public void testGetBalance() throws Exception{
-        Account account = new Account(10000);
         assertEquals(10000, account.getBalance());
 
         account = new Account(1000);
@@ -26,14 +33,12 @@ public class AccountTest {
 
     @Test
     public void testDeposit() throws Exception{
-        Account account = new Account(10000);
         account.deposit(1000);
         assertEquals(11000, account.getBalance());
     }
 
     @Test
     public void testWithdraw() throws Exception{
-        Account account = new Account(10000);
         account.withdraw(1000);
         assertEquals(9000, account.getBalance());
     }
